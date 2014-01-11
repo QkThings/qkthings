9087d71cc3db09055ef26dc8bf0c18ce5d2379a3
Mario Ribeiro <mario.ribas@gmail.com>
2014-01-11 23:38:01 +0000

    update

--- deploy.py
+++ deploy.py
@@ -1,110 +1,62 @@
 #!/usr/bin/python
 
-import os, platform, shutil
-from optparse import OptionParser
+import os, platform, shutil, datetime, tarfile, distutils.core
+import subprocess
+import os, tarfile, datetime
 
-class Settings:
-	def __init__(self):
-		self.deploy_path = os.path.join(os.path.expanduser("~"),"QkThings")
+DEPLOY_DIR="~"
+QT_VER_STR="5.1"
 
-	def read(self, path):
-		if not os.path.exists(path):
-			print "Unable to open settings file: %s" % path
-			return False
-		else:
-			f = open(path,"r")
-			content = f.readlines()
-			for line in content:
-				key = line.split("=")[0]
-				value = line.split("=")[1].rstrip("\n")
-				if key == "DEPLOY_PATH":
-					print key, value
-					self.deploy_path = os.path.expanduser(value);
-			f.close()
-			return True
-
-	def validate(self):
-#		if not os.path.exists(self.deploy_path):
-#			return False
-		return True
-
-	def show(self):
-		print "DEPLOY_PATH  = %s" % self.deploy_path
-		print "EMB_INC_PATH = %s" % self.emb_inc_path
-		print "EMB_LIB_PATH = %s" % self.emb_lib_path
-
-	def apply(self):
-		if not os.path.exists(self.deploy_path):
-			os.makedirs(self.deploy_path)
-		self.emb_inc_path = os.path.join(self.deploy_path,"embedded","include")
-		self.emb_lib_path = os.path.join(self.deploy_path,"embedded","lib")
-
-def createFolder(path):
-	if not os.path.exists(path):
-		os.makedirs(path)
-
-def createFolders(settings):
-	print "Creating folders..."
-	root = settings.deploy_path
-	if os.path.isdir(root):
-		shutil.rmtree(root)
-	createFolder(settings.deploy_path)
-	createFolder(settings.emb_inc_path)
-	createFolder(settings.emb_lib_path)
-
-def copyFile(src,dest):
-	fr = open(src, "r");
-	fw = open(dest, "w");
-	fw.write(fr.read())
-	fr.close()
-	fw.close()
-
-def deployEmbedded(settings):
-	print "Deploying embedded components..."
-
-	for root, dirs, files in os.walk("embedded"):
-		if "include" in root:
-			for inc_file in files:
-				src = os.path.join(root, inc_file)
-				dest = os.path.join(settings.emb_inc_path, inc_file)
-				copyFile(src, dest)
-		if "lib" in root:
-			for lib_file in files:
-				src = os.path.join(root, lib_file)
-				dest = os.path.join(settings.emb_lib_path, lib_file)
-				copyFile(src, dest)
-
-def deploySoftware():
-	print "Deploying software apps..."
-
-def main():
-	print "---------------------------------------------------------------------"
-	print "QkThings (qk-things.com)"
-	print "Deploying on", platform.system(), platform.release()
-	print "---------------------------------------------------------------------"
-
-	settings = Settings()
-	parser = OptionParser()
-	parser.add_option("-q", "--quiet",
-		              action="store_false", dest="verbose", default=True,
-		              help="don't print status messages to stdout")
-	(options, args) = parser.parse_args()
-
-			
-	if settings.validate() is False:
-		print "Invalid settings"
-		exit(1)
+def make_tarfile(source_dir, output_filename):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
 		
-	settings.apply()
-	settings.show()
+def main():
+	output_dir = os.path.basename("qkthings_backup")
+	date_and_ext = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".tar.gz"
+	folders = ["qkthings", "qkthings_linux"]
 
-	print "---------------------------------------------------------------------"
-	createFolders(settings)
-	deployEmbedded(settings)
-	deploySoftware()
-	print "---------------------------------------------------------------------"
+	print "Creating qkthings backup on folder: " + output_dir
+	for folder in folders:
+		print folder, "..."
+		make_tarfile(folder, os.path.join(output_dir, folder + "_" + date_and_ext))
 	print "Done"
+	
+def main():
+	os_name = platform.system();
+	os_str = ""
+	if os_name == "Windows":
+		os_str = "win"
+	elif os_name == "Linux":
+		os_str = "linux"
+	else:
+		print "OS not supported"
+		return
+	
+	print "DEPLOY qkthings for %s %s %s" % (os_name, platform.release(), platform.version())
+	print "The whole process may take a while. Be patient."
+	
+	deploy_dir = os.path.expanduser(DEPLOY_DIR)
+	qkthings_dir = os.path.join(deploy_dir, "qkthings")
+	qkide_dir = os.path.join(qkthings_dir, "qkide")
+	
+	if os.path.exists(qkthings_dir):
+		print "Cleaning..."
+		distutils.dir_util.remove_tree(qkthings_dir)
+		
+	print "Deploying qkide..."
+	distutils.dir_util.copy_tree("software/shared/qt/" + QT_VER_STR + "/" + os_str, qkide_dir)
+	distutils.dir_util.copy_tree("software/qkide/release", qkthings_dir + "/qkide")
+	
+	print "Deploying toolchain..."
+	distutils.dir_util.copy_tree("embedded/shared/toolchain/common", qkide_dir + "/resources/embedded/toolchain/common")
+	distutils.dir_util.copy_tree("embedded/shared/toolchain/" + os_str, qkide_dir + "/resources/embedded/toolchain/" + os_str)
 
+	
 if __name__ == "__main__":
-	main()
+	main();
+
+
+
+
 
